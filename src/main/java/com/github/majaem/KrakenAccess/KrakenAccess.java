94d5d317e44e9b016fdb048d51ea844aed2f928e
Marco <Marco@DESKTOP-JQ4RQ9Q>
2017-06-06 22:12:35 +0200

    Updated Structur. - Implemented ServerTimeRequest

--- src/main/java/com/github/majaem/KrakenAccess/KrakenAccess.java
+++ src/main/java/com/github/majaem/KrakenAccess/KrakenAccess.java
@@ -1,8 +1,13 @@
 package com.github.majaem.KrakenAccess;
 
+import com.github.majaem.KrakenAccess.requests.ServerTimeRequest;
+import com.github.majaem.KrakenAccess.responses.results.ServerTimeResult;
+
 public class KrakenAccess {
 
 	public static void main(String[] args) {
-			System.out.println("Hello World");
+		// Main Method for Test until Requests/Responses are implemented
+		System.out.println(new ServerTimeRequest().send());
+//		com.github.majaem.KrakenAccess.responses.results.Result x = new ServerTimeResult();
 	}
 }
